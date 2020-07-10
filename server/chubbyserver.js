e2f6dec0e0b7f1cd3a5d9a7bef3f4ccc5d13c5bd
Lin, Song <koalainuwo@hotmail.com>
2020-07-11 07:13:23 +0800

    git updates

--- server/chubbyserver.js
+++ server/chubbyserver.js
@@ -1,17 +1,17 @@
-var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 const fs = require('fs');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
-
+app.use(cors());
 app.get('/', function (req, res) {
    res.send('404');
 })
 
 app.get('/getLangList', function(req, res){
-    var rawdata = fs.readFileSync('langList.json');
+    var rawdata = fs.readFileSync('./data/langList.json');
     res.setHeader('Content-Type', 'application/json');
     res.end(rawdata);
 });
